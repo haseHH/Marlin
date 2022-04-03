95f0365ecf56e37992f7fcf8780670e49b85b665
haseHH <christian@hase.hamburg>
2022-04-03 14:16:16 +0200

    remove default z probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1182,7 +1182,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, 9, -5 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, 9, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
